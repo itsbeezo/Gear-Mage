22e91fbc1f42d6d4e9b434965471081caf4a657f
Beezo <Brian.dono12@gmail.com>
2026-06-12 21:17:16 -0400

    Automatic Clicker
    Clicker triggers automatically, but gears still dont trigger each other

--- Assets/Scripts/GearRotate.cs
+++ Assets/Scripts/GearRotate.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GearRotate : MonoBehaviour
 {
@@ -7,25 +8,35 @@ public class GearRotate : MonoBehaviour
 
     public GameObject Gear;
 
+    private float tickTimer = 0;
+
     [Header("Gear Settings")]
     [SerializeField]
-    public float RotationSpeed = 50;
+    public float RotationSpeed = 50f;
 
+    [Header("Production Settings")]
+    [SerializeField]
+    public float totalRotations;
+    public float rotationPerTick = .25f;
+    public float rotationInterval = 1f;
+    public float tickProgress = 0f;
 
-    private void Awake()
-    {
-        //gear =GameObject.
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+
+        tickTimer += Time.deltaTime;
+
+        tickProgress = Mathf.Clamp01(tickTimer / rotationInterval);
+
+        if (tickTimer >= rotationInterval)
+            {
+                tickTimer = 0;
+                totalRotations += 1;
+            ClickerRotation();
+            }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ClickerRotation();
@@ -51,7 +62,7 @@ public class GearRotate : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Clicker")
+        if (collision.gameObject.name == "Gear")
         {
             Debug.Log("Stay");
         }
@@ -108,4 +119,10 @@ public class GearRotate : MonoBehaviour
         Debug.Log("Gear Rotated");
         Gear.transform.Rotate(0, 0, -90);
     }
+
+    public void ClickerSpeed()
+    {
+
+    }
+
 }
