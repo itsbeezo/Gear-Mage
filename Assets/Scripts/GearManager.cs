0abb89d88b13b6d97664071e416ca8111193b289
Brian Dono <Brian.dono12@gmail.com>
2026-08-08 02:12:13 -0400

    Update: Fixed clicker and gear in grid

--- Assets/Scripts/GearManager.cs
+++ Assets/Scripts/GearManager.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class GearManager : MonoBehaviour
 {
     public static GearManager instance { get; private set; }
-    private int[,] GearMatrix = { {0, 0, 2, 6, 5, 0, 0, 0, 0, 0},
-                                  {0, 0, 0, 0, 1, 3, 3, 0, 0, 0},
-                                  {0, 0, 2, 1, 2, 4, 0, 0, 0, 0},
-                                  {0, 0, 0, 3, 4, 0, 0, 7, 8, 0},
-                                  {0, 0, 0, 4, 4, 0, 8, 8, 0, 0} };
+    private int[,] GearMatrix = { {0, 0, 8, 8, 8, 0, 0, 5, 3, 0},
+                                  {0, 8, 8, 8, 3, 8, 8, 6, 2, 0},
+                                  {0, 7, 0, 8, 7, 8, 2, 5, 6, 0},
+                                  {0, 8, 0, 8, 2, 8, 4, 3, 1, 0},
+                                  {0, 8, 8, 8, 8, 8, 8, 2, 1, 0} };
     [SerializeField] private List<GameObject> GearList;
     [SerializeField] private List<GearBox> GearBoxList1;
     [SerializeField] private List<GearBox> GearBoxList2;
