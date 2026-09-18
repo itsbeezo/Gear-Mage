7e565d76d66aa8c2538507607f27f23dc338a3de
Brian Dono <Brian.dono12@gmail.com>
2026-09-18 02:29:29 -0400

    Consume inventory stock on placement, reject drop when out of stock

--- Assets/Scripts/UIGearSlot.cs
+++ Assets/Scripts/UIGearSlot.cs
@@ -6,23 +6,30 @@ public class UIGearSlot : MonoBehaviour, IDropHandler
     public bool isFull = false;
     public void OnDrop(PointerEventData eventData)
     {
-        if (eventData.pointerDrag != null && !isFull)
+        if (eventData.pointerDrag == null || isFull) return;
+
+        GameObject draggedObject = eventData.pointerDrag;
+        UIDragHandler uiDragHandler = draggedObject.GetComponent<UIDragHandler>();
+
+        if (!uiDragHandler.startedOnBoard)
         {
-            isFull = true;
+            if (InventoryManager.instance != null &&
+                !InventoryManager.instance.TryConsume(uiDragHandler.gearNum))
+            {
+                // Out of stock - reject the drop. isConnected stays false,
+                // so OnEndDrag's existing catch-all branch snaps the dragged
+                // object back to its original position - no new bounce-back
+                // code needed here.
+                return;
+            }
+        }
 
-            GameObject draggedObject = eventData.pointerDrag;
+        isFull = true;
+        uiDragHandler.isConnected = true;
 
-            UIDragHandler uiDragHandler = draggedObject.GetComponent<UIDragHandler>();
-
-            uiDragHandler.isConnected = true;
-            Debug.Log(uiDragHandler.isConnected);
-
-            GearBox thisGearBox = gameObject.GetComponent<GearBox>();
-
-            GearManager.instance.SetGear(thisGearBox.GetXIndex(), thisGearBox.GetYIndex(), uiDragHandler.gearNum);
-
-            GearManager.instance.SpawnSingleGear(thisGearBox.GetXIndex(), thisGearBox.GetYIndex(), uiDragHandler.gearNum); 
-        } 
+        GearBox thisGearBox = gameObject.GetComponent<GearBox>();
+        GearManager.instance.SetGear(thisGearBox.GetXIndex(), thisGearBox.GetYIndex(), uiDragHandler.gearNum);
+        GearManager.instance.SpawnSingleGear(thisGearBox.GetXIndex(), thisGearBox.GetYIndex(), uiDragHandler.gearNum);
     }
 
     public void ClearSlot()
